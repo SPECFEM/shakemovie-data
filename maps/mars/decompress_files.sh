9cce41b7c490bebef2baa8f9493d3d0bd1ebcf12
Daniel Peter <daniel.peter22@gmail.com>
2023-03-21 18:26:55 +0100

    adds mars maps

---
+++ maps/mars/decompress_files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo
+echo "decompressing files"
+echo
+
+tar xvjf clouds_16384.tga.tar.bz2
+tar xvjf mars_16384.tga.tar.bz2
+tar xvjf mars_topo_16384.ppm.tar.bz2
+tar xvjf mars_viking.tga.tar.bz2
+
+
